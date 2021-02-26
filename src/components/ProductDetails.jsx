bd5bbd8f3791242e05affd9c7204792431cd0d00
Atik1000 <foysalkazi45@gmail.com>
2021-02-26 20:29:33 +0600

    added

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -2,14 +2,11 @@ import React from "react";
 
 const ProductDetails = (props) => {
   console.log(props.detail);
-  //   console.log(detail);
-  // const {description,category}=props.detail;
+  
   return (
     <div>
-      {props.detail.category}
-      {/* {category} */}
-      {/* <h1>{detail.description}</h1> */}
-      {/* <h1>{props.pd.description}</h1> */}
+      {/* {props.detail.category} */}
+    
     </div>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,51 +3,53 @@ import "./Product.css";
 import ProductDetails from "./ProductDetails";
 
 const Products = () => {
-    const product=[
+    const [product, setProduct] = useState([
         {
-          name: "JavaScript",
-          description: "Programming language.Which is best for web app developed.",
-          price: "$10.50",
-          category: "Web app",
+            name: "JavaScript",
+            description:
+                "Programming language.Which is best for web app developed.",
+            price: "$10.50",
+            category: "Web app",
+            id:1
         },
         {
-          name: "Python",
-          description: "Programming language.Which is best for Data Science",
-          price: "$20.10",
-          category: "Data science",
+            name: "Python",
+            description: "Programming language.Which is best for Data Science",
+            price: "$20.10",
+            category: "Data science",
+            id:2
         },
         {
-          name: "Java",
-          description:
-            "Programming language.Which is best for mobile app developed.",
-          price: "$30.10",
-          category: "Android App",
+            name: "Java",
+            description:
+                "Programming language.Which is best for mobile app developed.",
+            price: "$30.10",
+            category: "Android App",
+            id:3
         },
         {
-          name: "C++",
-          description: "Programming language.Which is best for  game developed.",
-          price: "$30.10",
-          category: "game App",
+            name: "C++",
+            description:
+                "Programming language.Which is best for  game developed.",
+            price: "$30.10",
+            category: "game App",
+            id:4
         },
-      ]
-//   const [product, setProduct] = useState();
-  console.log(product);
-  return (
-    // <div>
-    //     {product.map((pd) => (
-    //         <div className="product">
-    //             {/* <p>{pd.name}</p>
-    //             <p> {pd.price}</p> */}
-    //             <ProductDetails detail={pd}/>
-    //         </div>
-    //     ))}
-    // </div>
-    <div>
-      {product.map((pd) => (
-        <ProductDetails detail={pd} />
-      ))}
-    </div>
-  );
+    ]);
+
+    return (
+    
+        <div>
+                <ProductDetails detail={product}/>
+            {product.map((pd) => (
+                <div className="product">
+                    <p>{pd.name}</p>
+                    <p> {pd.price}</p>
+                    {/* <ProductDetails detail={pd}/> */}
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default Products;
